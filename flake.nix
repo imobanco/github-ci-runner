0e898bcee52a224cc77495ea7db6c0b148716ace
Pedro O. A. Regis <pedroalencarregis@hotmail.com>
2024-01-22 16:24:06 -0300

    Debugando nix profile install nixpkgs#nodejs

--- flake.nix
+++ flake.nix
@@ -355,7 +355,7 @@
                 # DeviceAllow = [ "/dev/kvm" ];
                 # Environment = "PATH=/run/current-system/sw/bin:${lib.makeBinPath [ pkgs.iputils ]}"; # https://discourse.nixos.org/t/how-to-add-path-into-systemd-user-home-manager-service/31623/4
                 # Environment = "PATH=/run/current-system/sw/bin:/run/wrappers/bin:/home/nixuser/.nix-profile"; # https://discourse.nixos.org/t/how-to-add-path-into-systemd-user-home-manager-service/31623/4
-                Environment = "PATH=/run/current-system/sw/bin:/run/wrappers/bin:/nix/var/nix/profiles/per-user/nixuser/profile"; # https://discourse.nixos.org/t/how-to-add-path-into-systemd-user-home-manager-service/31623/4
+                Environment = "PATH=/run/current-system/sw/bin:/run/wrappers/bin:/nix/var/nix/profiles/per-user/nixuser/profile/bin:/home/nixuser/.nix-profile/bin"; # https://discourse.nixos.org/t/how-to-add-path-into-systemd-user-home-manager-service/31623/4
               };
 
               virtualisation.docker.enable = true;
